1a53b4830996aa3a3e87967f74b565dd47a7a3c5
Naresh-Agra <Naresh-Agra@users.noreply.github.com>
2019-09-19 18:52:10 +0530

    Initializing Spark

--- Spark_Core/src/main/scala/Spark_Core/Spark_Core.scala
+++ Spark_Core/src/main/scala/Spark_Core/Spark_Core.scala
@@ -5,5 +5,14 @@ import org.apache.spark.sql.SparkSession
 
 
 object Spark_Core {
-  
+  def main(args:Array[String]):Unit={
+    
+    val conf = new SparkConf().setAppName("SarkCore").setMaster("local[*]")
+    val sc=new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+    
+    
+    
+    
+  }
 }
